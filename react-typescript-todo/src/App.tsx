5588a3e5dd3471a7a0b01e12db2ff0e6e5874d9f
sandeshjadhav5 <jadhavs932@gmail.com>
2023-08-16 22:56:03 +0530

    added inputfield

--- react-typescript-todo/src/App.tsx
+++ react-typescript-todo/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import InputField from "./Components/InputField";
 
 import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Todo App</h1>
-    </div>
+    <>
+      <h1>TODO PLANNER</h1>
+      <div>
+        <InputField />
+      </div>
+    </>
   );
 };
 
---
+++ react-typescript-todo/src/Components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputField: React.FC = () => {
+  return (
+    <>
+      <form>
+        <input />
+      </form>
+    </>
+  );
+};
+
+export default InputField;
