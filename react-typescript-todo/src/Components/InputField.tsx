631815659a06b1503fde7082e2256f8562307069
sandeshjadhav5 <jadhavs932@gmail.com>
2023-08-16 23:23:33 +0530

    added styles.css

--- react-typescript-todo/src/Components/InputField.tsx
+++ react-typescript-todo/src/Components/InputField.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./styles.css";
 
 const InputField: React.FC = () => {
   return (
     <>
-      <form>
-        <input />
+      <form className="input_Form">
+        <input className="input_box" type="input" placeholder="Enter a task" />
+
+        <button type="submit" className="input_submit">
+          Add
+        </button>
       </form>
     </>
   );
